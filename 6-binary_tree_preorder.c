5e1ea93cdd52f51ce2413bea047773ec2d2c1a09
jennycode7 <jennessecosmas7@gmail.com>
2024-06-22 04:11:50 -0700

    binary tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_preorder - prints tree by preoder method
+ * @tree: pointer to root node
+ * @func: function that prints node data
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	func(tree->n);
+	if (tree->left)
+	{
+		binary_tree_preorder(tree->left, func);
+	}
+	if (tree->right)
+	{
+		binary_tree_preorder(tree->right, func);
+	}
+}
