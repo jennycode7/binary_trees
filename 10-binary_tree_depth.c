1de9fc6d1a235303154610461317cab4e6f921fe
jennycode7 <jennessecosmas7@gmail.com>
2024-06-30 09:09:32 -0700

    binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		l = tree->parent ? 1 + binary_tree_depth : 0;
+		size_t l = 0;
+		l = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 		return (l);
 	}
 	return (0);
