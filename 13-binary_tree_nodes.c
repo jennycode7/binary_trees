9f88761dfa632f5a22606a0f7726146139d8031d
jennycode7 <jennessecosmas7@gmail.com>
2024-06-30 22:42:54 -0700

    binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
 		return (1 + binary_tree_nodes(tree->left)
 				+ binary_tree_nodes(tree->right));
