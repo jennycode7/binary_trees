e7ffbabbe2605b4d73e79941f2f848531726faca
jennycode7 <jennessecosmas7@gmail.com>
2024-06-30 22:14:01 -0700

    binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		{
 			return (1);
 		}
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right))
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	}
 
 	return (0);
