9e0b87bbabb063abf77d3635a9f54555595e464d
jennycode7 <jennessecosmas7@gmail.com>
2024-06-30 22:39:22 -0700

    binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_nodes - counts nodes with child
+ * @tree: pointer to node
+ * Return: size_t
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL || tree->right == NULL)
+	{
+		return (1 + binary_tree_nodes(tree->left)
+				+ binary_tree_nodes(tree->right));
+	}
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
