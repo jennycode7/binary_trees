f98c21d66eb874cbeb205a1777ec5e2f29286257
jennycode7 <jennessecosmas7@gmail.com>
2024-07-12 17:54:04 -0700

    binary tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *child_node = malloc(sizeof(binary_tree_t));
+
+	if (child_node == NULL)
+	{
+		return (NULL);
+	}
+	child_node->n = value;
+	child_node->left = NULL;
+	child_node->right = NULL;
+	child_node->parent = parent;
+
+	return (child_node);
+}
 
 /**
  * bst_insert_recursion - recursively traverses a Binary Search Tree to insert
